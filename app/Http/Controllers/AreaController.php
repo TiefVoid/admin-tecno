3522576c6ac5b38a953d5b660f5d30009135c88f
Paloma Rod <placeholder.prod@outlook.com>
2022-05-11 18:58:44 -0500

    Update AreaController.php

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -19,11 +19,6 @@ class AreaController extends Controller
             $query->Where('nombre','like','%'.$data['nombre'].'%');
         }
 
-        if($request->has('pageNumber')){
-            $offset = ($data['pageNumber']-1)*15;
-            $query->skip($offset)->take(15);
-        }
-
         return $query->get();
     }
 
